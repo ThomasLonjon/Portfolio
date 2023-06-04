654397405061858b7e1cf371cef1f47ba4f81842
ThomasLonjon <lonjon.thomas@gmail.com>
2023-06-04 14:24:50 +0200

    added route

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,37 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import Draggable from "react-draggable";
 import MyMap from "./components/MyMap";
 import "./App.scss";
 import mapboxgl from "mapbox-gl";
 
 function App() {
-  // ---------------------------------------------UseState-----------------------------------------
+  // -------------------------------------------UseStates-----------------------------------------
 
   const [position1, setPosition1] = useState({
+    mapNumber: 1,
     lat: -0.1,
     lng: 47,
     zoom: 5.1,
     pitch: 0,
-    isClicked: false,
+    isChosen: false,
   });
 
   const [position2, setPosition2] = useState({
+    mapNumber: 2,
     lat: 9.12,
     lng: 42.21,
     zoom: 6.9,
     pitch: 0,
-    isClicked: false,
+    isChosen: false,
   });
 
   const [position3, setPosition3] = useState({
+    mapNumber: 3,
     lat: 0.9,
     lng: 47,
     zoom: 7.1,
     pitch: 0,
-    isClicked: false,
+    isChosen: false,
   });
 
   const [positionGeoCoder, setPositionGeoCoder] = useState({
@@ -37,62 +40,34 @@ function App() {
   });
 
   const [isClicked, setIsClicked] = useState({
-    1: false,
-    2: false,
-    3: false,
+    first: false,
+    second: false,
+    third: false,
   });
 
-  // const [latitude, setLatitude] = useState(-0.1);
-  // const [longitude, setLongitude] = useState(47);
-
-  // const [latitude1, setLatitude1] = useState(-0.1);
-  // const [longitude1, setLongitude1] = useState(47);
-
-  // const [latitude2, setLatitude2] = useState(9.14);
-  // const [longitude2, setLongitude2] = useState(42.21);
-  // const [latitude3, setLatitude3] = useState(0.9);
-  // const [longitude3, setLongitude3] = useState(47);
-  // const [isChosen1, setIsChosen1] = useState(false);
-  // const [isChosen2, setIsChosen2] = useState(false);
-  // const [isChosen3, setIsChosen3] = useState(false);
-  // const [zoom1, setZoom1] = useState(5.1);
-  // const [zoom2, setZoom2] = useState(6.9);
-  // const [zoom3, setZoom3] = useState(7.1);
-  // const [pitch1, setPitch1] = useState(0);
-  // const [pitch2, setPitch2] = useState(0);
-  // const [pitch3, setPitch3] = useState(0);
-
-  // const [isClicked1, setIsClicked1] = useState(false);
-  // const [isClicked2, setIsClicked2] = useState(false);
-  // const [isClicked3, setIsClicked3] = useState(false);
-
-  // ---------------------------------------------HandleClick-----------------------------------------
+  // ---------------------------------------------HandleClick Button-----------------------------------------
 
   const handleClick1 = () => {
-    setIsClicked({ 1: true, 2: false, 3: false });
+    setIsClicked({ first: true, second: false, third: false });
   };
 
   const handleClick2 = () => {
-    setIsClicked({ 1: false, 2: true, 3: false });
+    setIsClicked({ first: false, second: true, third: false });
   };
 
   const handleClick3 = () => {
-    setIsClicked({ 1: false, 2: false, 3: true });
+    setIsClicked({ first: false, second: false, third: true });
   };
 
-  // ---------------------------------------------Handlechange-----------------------------------------
+  // ----------------------------------------Handlechange Geocoder Input-----------------------------------------
 
   const handleChange = (result) => {
     setPositionGeoCoder({
       lat: result.center[0],
       lng: result.center[1],
     });
-    // setLatitude(result.center[0]);
-    // setLongitude(result.center[1]);
-    // setIsChosen1(true);
-    // setZoom1(15.1);
-    // setPitch1(50);
   };
+  // -----------------------------------Handlechange Set Map Position -----------------------------------------
 
   const handleChange1 = (a, b) => {
     setPosition1({
@@ -106,20 +81,26 @@ function App() {
   };
 
   const handleChange2 = (a, b) => {
-    setLatitude2(a);
-    setLongitude2(b);
-    setIsChosen2(true);
-    setZoom1(15.1);
-    setPitch1(50);
+    setPosition2({
+      ...position2,
+      lat: a,
+      lng: b,
+      isChosen: true,
+      zoom: 15.1,
+    });
   };
 
   const handleChange3 = (a, b) => {
-    setLatitude3(a);
-    setLongitude3(b);
-    setIsChosen3(true);
-    setZoom1(15.1);
-    setPitch1(50);
+    setPosition3({
+      ...position3,
+      lat: a,
+      lng: b,
+      isChosen: true,
+      zoom: 9,
+    });
   };
+
+  const [route, setRoute] = useState({});
 
   // ---------------------------------------------Geocoder-----------------------------------------
 
@@ -132,8 +113,6 @@ function App() {
     geocoder.addTo("#geocoder");
   }, []);
 
-  // --------------------------------------------------------------------------------
-
   useEffect(() => {
     geocoder.on("result", (e) => {
       handleChange(e.result);
@@ -142,16 +121,27 @@ function App() {
   });
 
   useEffect(() => {
-    if (isClicked1) {
-      handleChange1(latitude, longitude);
+    if (isClicked.first) {
+      handleChange1(positionGeoCoder.lat, positionGeoCoder.lng);
     }
-    if (isClicked2) {
-      handleChange2(latitude, longitude);
+    if (isClicked.second) {
+      handleChange2(positionGeoCoder.lat, positionGeoCoder.lng);
     }
-    if (isClicked3) {
-      handleChange3(latitude, longitude);
+    if (isClicked.third) {
+      handleChange3(positionGeoCoder.lat, positionGeoCoder.lng);
     }
-  }, [latitude, longitude]);
+  }, [positionGeoCoder.lat, positionGeoCoder.lng]);
+
+  // ---------------------------------------- getDirection----------------------------------------
+
+  useEffect(() => {
+    fetch(
+      "https://api.mapbox.com/directions/v5/mapbox/cycling/48,2;45,4?geometries=geojson&access_token=pk.eyJ1IjoidGhvbWFzbG9uam9uIiwiYSI6ImNsaThwNTFnYzFsd3ozZnBjczN3aDlhYzcifQ.na2-On5k8L1PUKU8Em_-Ew"
+    )
+      .then((response) => response.json())
+      .then((data) => setRoute(data.routes[0].geometry.coordinates))
+      .catch((err) => console.error(err));
+  }, []);
 
   // ---------------------------------------------Return---------------------------------------------
 
@@ -166,18 +156,8 @@ function App() {
       <Draggable cancel="strong">
         <div className="card1">
           <h3>EXPLOREZ DE NOUVELLES EXPERIENCES CARTOGRAPHIQUES</h3>
-          <strong>
-            <MyMap
-              latitude={latitude1}
-              longitude={longitude1}
-              zoom={zoom1}
-              pitch={pitch1}
-              isChosen={isChosen1}
-            />
-          </strong>
-          <p className="credits">
-            Crédits : mapbox, openstreetMap, Madison Draper
-          </p>
+          <MyMap {...position1} route={route} />
+          <p className="credits">Crédits : mapbox, openstreetMap, Madison Draper</p>
         </div>
       </Draggable>
 
@@ -186,18 +166,8 @@ function App() {
           <div className="cursor">
             <h3>PRÉPAREZ VOTRE EXPÉDITION</h3>
           </div>
-          <strong className="no-cursor">
-            <MyMap
-              latitude={latitude2}
-              longitude={longitude2}
-              zoom={zoom2}
-              pitch={pitch2}
-              isChosen={isChosen2}
-            />
-          </strong>
-          <p className="credits">
-            Crédits : mapbox, openstreetMap, Madison Draper
-          </p>
+          <MyMap {...position2} route={route} />
+          <p className="credits">Crédits : mapbox, openstreetMap, Madison Draper</p>
         </div>
       </Draggable>
 
@@ -206,18 +176,8 @@ function App() {
           <div className="cursor">
             <h3>CHANGEZ D&apos;ÉCHELLE</h3>
           </div>
-          <strong className="no-cursor">
-            <MyMap
-              latitude={latitude3}
-              longitude={longitude3}
-              zoom={zoom3}
-              pitch={pitch3}
-              isChosen={isChosen3}
-            />
-          </strong>
-          <p className="credits">
-            Crédits : mapbox, openstreetMap, Madison Draper
-          </p>
+          <MyMap {...position3} route={route} />
+          <p className="credits">Crédits : mapbox, openstreetMap, Madison Draper</p>
         </div>
       </Draggable>
 
@@ -226,22 +186,13 @@ function App() {
           <div className="cursor">
             <h3>CHOISISSEZ VOTRE CARTE</h3>
             <div className="buttonContainer">
-              <strong
-                className={isClicked1 ? "clickedButton" : "button"}
-                onClick={handleClick1}
-              >
+              <strong className={isClicked.first ? "clickedButton" : "button"} onClick={handleClick1}>
                 Carte 1
               </strong>
-              <strong
-                className={isClicked2 ? "clickedButton" : "button"}
-                onClick={handleClick2}
-              >
+              <strong className={isClicked.second ? "clickedButton" : "button"} onClick={handleClick2}>
                 Carte 2
               </strong>
-              <strong
-                className={isClicked3 ? "clickedButton" : "button"}
-                onClick={handleClick3}
-              >
+              <strong className={isClicked.third ? "clickedButton" : "button"} onClick={handleClick3}>
                 Carte 3
               </strong>
             </div>
--- src/components/MyMap.jsx
+++ src/components/MyMap.jsx
@@ -1,15 +1,22 @@
 import PropTypes from "prop-types";
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import mapboxgl from "mapbox-gl";
 
 mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
 
-// ---------------------------------------- FUNCTION----------------------------------------
-
-function MyMap({ latitude, longitude, zoom, pitch, isChosen }) {
+function MyMap({ mapNumber, lat, lng, zoom, pitch, isChosen, route }) {
   const mapContainer = useRef(null);
   const map = useRef(null);
+  const [geojson, setGeojson] = useState({
+    type: "Feature",
+    properties: {},
+    geometry: {
+      type: "LineString",
+      coordinates: [],
+    },
+  });
 
+  // ---------------------------------------- Add map----------------------------------------
   useEffect(() => {
     if (map.current) {
       map.current.remove(); // Supprime l'ancienne instance de la carte
@@ -18,16 +25,18 @@ function MyMap({ latitude, longitude, zoom, pitch, isChosen }) {
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
       style: "mapbox://styles/thomaslonjon/clhz4ydld02dr01pg5mdkaycb",
-      center: [latitude, longitude],
+      center: [lat, lng],
       zoom: zoom,
       antialias: true,
     });
   }, []);
 
+  // ---------------------------------------- FlyTo----------------------------------------
+
   useEffect(() => {
     if (isChosen) {
       map.current.flyTo({
-        center: [latitude, longitude],
+        center: [lat, lng],
         essential: true,
         duration: 9000,
         zoom: zoom,
@@ -35,18 +44,154 @@ function MyMap({ latitude, longitude, zoom, pitch, isChosen }) {
       });
       return;
     }
-  }, [latitude, longitude]);
+  }, [lat, lng]);
+
+  // ---------------------------------------- Route----------------------------------------
+
+  useEffect(() => {
+    map.current.on("load", () => {
+      map.current.addSource("route", {
+        type: "geojson",
+        data: {
+          type: "FeatureCollection",
+          features: [],
+        },
+      });
+
+      map.current.addLayer({
+        id: "route",
+        type: "line",
+        source: "route",
+        layout: {
+          "line-join": "round",
+          "line-cap": "round",
+        },
+        paint: {
+          "line-color": "#cc6552",
+          "line-width": 5,
+          "line-opacity": 0.9,
+        },
+      });
+
+      getRoute();
+    });
+
+    async function getRoute() {
+      await fetch(
+        "https://api.mapbox.com/directions/v5/mapbox/cycling/2,48;2,45?geometries=geojson&overview=full&steps=true&access_token=pk.eyJ1IjoidGhvbWFzbG9uam9uIiwiYSI6ImNsaThwNTFnYzFsd3ozZnBjczN3aDlhYzcifQ.na2-On5k8L1PUKU8Em_-Ew"
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          map.current.getSource("route").setData({
+            type: "Feature",
+            properties: {},
+            geometry: {
+              type: "LineString",
+              coordinates: data.routes[0].geometry.coordinates,
+            },
+          });
+        })
+        .catch((err) => console.error(err));
+    }
+  }, []);
+  // -----------------------------------------------
+  // useEffect(() => {
+  //   // if (mapNumber === 3) {
+  //   // map.current.addControl(
+  //   //   new MapboxDirections({
+  //   //     accessToken: mapboxgl.accessToken,
+  //   //   }),
+  //   //   "top-left"
+  //   // );
+
+  //   const geojson = {
+  //     type: "Feature",
+  //     properties: {},
+  //     geometry: {
+  //       type: "LineString",
+  //       coordinates: [
+  //         [-122.483696, 37.833818],
+  //         [-122.483482, 37.833174],
+  //         [-122.483396, 37.8327],
+  //         [-122.483568, 37.832056],
+  //         [-122.48404, 37.831141],
+  //         [-122.48404, 37.830497],
+  //         [-122.483482, 37.82992],
+  //         [-122.483568, 37.829548],
+  //         [-122.48507, 37.829446],
+  //         [-122.4861, 37.828802],
+  //         [-122.486958, 37.82931],
+  //         [-122.487001, 37.830802],
+  //         [-122.487516, 37.831683],
+  //         [-122.488031, 37.832158],
+  //         [-122.488889, 37.832971],
+  //         [-122.489876, 37.832632],
+  //         [-122.490434, 37.832937],
+  //         [-122.49125, 37.832429],
+  //         [-122.491636, 37.832564],
+  //         [-122.492237, 37.833378],
+  //         [-122.493782, 37.833683],
+  //       ],
+  //     },
+  //   };
+
+  //   console.log(geojson);
+
+  //   map.current.on("load", () => {
+  //     console.log("rendered");
+  //     if (map.current.getSource("route")) {
+  //       map.current.getSource("route").setData(geojson);
+  //     } else {
+  //       map.current.addLayer({
+  //         id: "route",
+  //         type: "line",
+  //         source: {
+  //           type: "geojson",
+  //           data: geojson,
+  //         },
+  //         layout: {
+  //           "line-join": "round",
+  //           "line-cap": "round",
+  //         },
+  //         paint: {
+  //           "line-color": "#3887be",
+  //           "line-width": 50,
+  //           "line-opacity": 0.75,
+  //         },
+  //       });
+  //     }
+  //   });
+  // });
+
+  // useEffect(() => {
+  //   const geojson = {
+  //     type: "Feature",
+  //     properties: {},
+  //     geometry: {
+  //       type: "LineString",
+  //       coordinates: route,
+  //     },
+  //   };
+
+  //   if (map.current.getSource("route")) {
+  //     map.current.getSource("route").setData(geojson);
+  //   }
+  // }, [route]);
 
   // ---------------------------------------- RETURN----------------------------------------
 
-  return <div ref={mapContainer} className="map" />;
+  return (
+    <strong className="no-cursor">
+      <div ref={mapContainer} className="map" />
+    </strong>
+  );
 }
 
 export default MyMap;
 
 MyMap.propTypes = {
-  longitude: PropTypes.number.isRequired,
-  latitude: PropTypes.number.isRequired,
+  lng: PropTypes.number.isRequired,
+  lat: PropTypes.number.isRequired,
   pitch: PropTypes.number.isRequired,
   zoom: PropTypes.number.isRequired,
   isChosen: PropTypes.bool.isRequired,
