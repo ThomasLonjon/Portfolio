1d9a47aa987f5b6e68a81dadb7b01f1b5be72c09
ThomasLonjon <lonjon.thomas@gmail.com>
2023-06-26 13:39:43 +0200

    added loader

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,33 @@
+import { useState, useEffect } from "react";
+
 import Intro from "../../components/Intro/Intro";
 import About from "../../components/About/About";
 import Projects from "../../components/Projects/Projects";
 import Bordless from "../../assets/img/Bordless.png";
+import "./Home.scss";
 
 function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1500);
+  }, []);
+
   // ---------------------------------------------Return---------------------------------------------
 
   return (
-    <div className="background" style={{ backgroundImage: `url(${Bordless})`, backgroundSize: "cover" }}>
-      <Intro />
-      <About />
-      <Projects />
-    </div>
+    <>
+      <div className={isLoading ? "loader" : "loaderHidden"}>
+        <h1>Bienvenue sur mon Porfolio !</h1>
+      </div>
+      <div className="background" style={{ backgroundImage: `url(${Bordless})`, backgroundSize: "cover" }}>
+        <Intro />
+        <About />
+        <Projects />
+      </div>
+    </>
   );
 }
 
